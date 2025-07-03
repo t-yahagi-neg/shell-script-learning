e3f068ca9f06ae0fb24f4d6ef9fdad2470f8c792
yahagi <t.yahagi.ng@gmail.com>
2025-07-03 17:51:17 +0900

    文字列をシングルクォーテーションで囲む

--- test.sh
+++ test.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 var=value
-echo $var
-echo \$var	# $ をエスケープすることでパラメータ展開させない
-echo \$var $var	# 並べて出力
-echo \\		# \ 自身も \ でエスケープできる
-
-var=hoge\ fuga	# スペースをエスケープすることで連続した文字列として処理できる
-echo $var
+var2='${var1}'
+echo $var2
