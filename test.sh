fb9bd301a91732b4170ea9bcfb635c5734a2fb86
yahagi <t.yahagi.ng@gmail.com>
2025-07-03 17:42:44 +0900

    特殊文字とエスケープ

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-empty=''
-echo \$empty: $empty	# 後述のエスケープを使用
-empty=	# これも空文字を意味する
-echo \$empty: $empty
-echo \$undefined: $undefined
+var=value
+echo $var
+echo \$var	# $ をエスケープすることでパラメータ展開させない
+echo \$var $var	# 並べて出力
+echo \\		# \ 自身も \ でエスケープできる
+
+var=hoge\ fuga	# スペースをエスケープすることで連続した文字列として処理できる
+echo $var
