7cffe73a5e815655d26146ecdc89e50df6f5502d
yahagi <t.yahagi.ng@gmail.com>
2025-07-03 17:29:55 +0900

    変数の代入

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo 'Hello world!'; pwd; echo 'End world!';
+var='value'	# = の周りにスペースを入れてはいけません
+echo $var	# 後述しますが $ は変数を参照する時につける記号です
+
+var='change'	# 定義済みの変数の値を変更することも可能です
+echo $var
