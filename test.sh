d9ef856ee34d99f97823ec6e34c64a2a048e350e
yahagi <t.yahagi.ng@gmail.com>
2025-07-04 10:23:35 +0900

    ログ表示追加

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-var1='hoge''fuga'
-echo '$var1': $var1
+# 色の定義
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'	# No Color
 
-var2=hoge
-var3=$var1$var2
-echo '$var3': $var3
+# ログ関数
+log_info()	{ echo -e "${GREEN}[INFO] $1${NC}"; }
+log_warn()	{ echo -e "${YELLOW}[WARN] $1${NC}"; }
+log_error()	{ echo -e "${RED}[ERROR] $1${NC}"; }
 
-# 以下は $var3 と同じ結果になるでしょうか？
-echo "\$var1hoge": $var1hoge
-echo "\$var1'hoge'": $var1'hoge'
-echo "\${var1}hoge": ${var1}hoge
-echo "\${var1}'hoge'": ${var1}'hoge'
+# 本体処理
+log_info "スクリプト開始"
+log_warn "これは注意メッセージです"
+log_error "これはエラーメッセージです"
+log_info "スクリプト終了"
