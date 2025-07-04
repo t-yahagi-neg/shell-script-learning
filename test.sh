57365eb0aeab063140861fc20ff0690a01fe908c
yahagi <t.yahagi.ng@gmail.com>
2025-07-04 09:35:38 +0900

    文字列の連結

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-var1=value
-var2="${var1}"
-echo $var2
+var1='hoge''fuga'
+echo '$var1': $var1
+
+var2=hoge
+var3=$var1$var2
+echo '$var3': $var3
+
+# 以下は $var3 と同じ結果になるでしょうか？
+echo "\$var1hoge": $var1hoge
+echo "\$var1'hoge'": $var1'hoge'
+echo "\${var1}hoge": ${var1}hoge
+echo "\${var1}'hoge'": ${var1}'hoge'
