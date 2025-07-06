44a6422c6da06bd27964f4063361e626f57e7d64
rhel <rhel@localhost.localdomain>
2025-07-07 02:09:42 +0900

    値の存在するインデックスの取得

--- RHEL/script_old/arr-del.sh
+++ RHEL/script_old/arr-del.sh
@@ -9,10 +9,17 @@ echo ${countries[1]}
 echo ${countries[2]}
 echo ${countries[3]}
 
+echo "すべての要素を参照"
 echo ${countries[@]}
 
 countries2=(aaa bbb "${countries[@]}")	# 配列の先頭に要素を追加
 countries3=("${countries[@]}" ccc ddd)	# 配列の末尾に要素を追加
+echo "先頭に要素を追加した配列"
 echo "${countries2[@]}"
-
+echo "末尾に要素を追加した配列"
 echo "${countries3[@]}"
+
+echo "値が存在するインデックスのみを取得"
+countries4=([0]=Japan [2]=France [3]=Germany)
+echo ${!countries4[@]}
+echo ${!countries[@]}
